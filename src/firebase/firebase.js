8e42b2e9d5e946ae25e99a3d4d370fd2ef3ec4c1
Safi Ullah <safi06947@gmail.com>
2024-09-05 17:07:49 +0500

    Firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9Too5Y9FtaNxjFdL5c7VsNxSQMRFMTWg",
+  authDomain: "wego-with-firebase-7acb8.firebaseapp.com",
+  projectId: "wego-with-firebase-7acb8",
+  storageBucket: "wego-with-firebase-7acb8.appspot.com",
+  messagingSenderId: "949029824948",
+  appId: "1:949029824948:web:5f2d884516f995ce176ae1"
+};
+
+
+const app = initializeApp(firebaseConfig);
